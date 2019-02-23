d24717d9b56df5aa7e10d42d3cc788996a3db173
Ashutosh Mishra <ashutoshdtu@gmail.com>
2019-02-23 15:44:50 +0530

    trying to fix import errors on rtd.org

--- src/eazyserver/rpc/__init__.py
+++ src/eazyserver/rpc/__init__.py
@@ -3,7 +3,8 @@ logger = logging.getLogger(__name__)
 logger.debug("Loaded " + __name__)
 
 from jsonrpcserver import methods
-from exceptions import *
+from .exceptions import *
 
-from ..rpc.influxdb_api import *
-from ..rpc.meta import *
+
+from .influxdb_api import *
+from .meta import *
